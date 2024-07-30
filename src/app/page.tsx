fc3f6277cbbe4728ca21e3028fffe4a99e0ad3b8
Dhvanit <monparadhvanit@gmail.com>
2024-07-30 13:28:57 +0530

    created hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-      <p>chai</p>
+      <HeroSection />
     </main>
   );
 }
